4b86716a4d6c97fba48d3c8721275a027f582d36
yxqsnz <bolaknt@gmail.com>
2022-02-13 18:34:38 -0300

    remove unless things from wrapper.h

--- wrapper.h
+++ wrapper.h
@@ -1,15 +1,4 @@
 #ifndef _3BC_WRAPPER_H_
 #include "3bc-lang/src/3bc.h"
-// #include "3bc-lang/src/3bc_macros.h"
-// #include "3bc-lang/src/3bc_errors.h"
-// #include "3bc-lang/src/3bc_types.h"
-// #include "3bc-lang/src/3bc_header.h"
-// #include "3bc-lang/src/3bc_register.h"
-// #include "3bc-lang/src/3bc_optional_custom.h"
-// #include "3bc-lang/src/3bc_optional_interpreter.h"
-
-// /** compatibility fixes **/
-// #include "3bc-lang/src/3bc_support.h"
-
 #define _3BC_WRAPPER_H_
 #endif
